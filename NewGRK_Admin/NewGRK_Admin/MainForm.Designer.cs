fb640899891c486b966b6f07b2007557ab3346af
deniska1812 <den93mos@yandex.ru>
2018-11-22 10:14:48 +0300

    Добавление формы создания GRK объекта

--- NewGRK_Admin/NewGRK_Admin/MainForm.Designer.cs
+++ NewGRK_Admin/NewGRK_Admin/MainForm.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCreateObject = new System.Windows.Forms.Button();
+            this.btnEditObject = new System.Windows.Forms.Button();
+            this.btnEtc = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCreateObject
+            // 
+            this.btnCreateObject.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateObject.Name = "btnCreateObject";
+            this.btnCreateObject.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCreateObject.Size = new System.Drawing.Size(100, 100);
+            this.btnCreateObject.TabIndex = 0;
+            this.btnCreateObject.Text = "Создать объект";
+            this.btnCreateObject.UseVisualStyleBackColor = true;
+            // 
+            // btnEditObject
+            // 
+            this.btnEditObject.Location = new System.Drawing.Point(118, 12);
+            this.btnEditObject.Name = "btnEditObject";
+            this.btnEditObject.Size = new System.Drawing.Size(100, 100);
+            this.btnEditObject.TabIndex = 1;
+            this.btnEditObject.Text = "Изменить объект";
+            this.btnEditObject.UseVisualStyleBackColor = true;
+            // 
+            // btnEtc
+            // 
+            this.btnEtc.Location = new System.Drawing.Point(224, 12);
+            this.btnEtc.Name = "btnEtc";
+            this.btnEtc.Size = new System.Drawing.Size(100, 100);
+            this.btnEtc.TabIndex = 2;
+            this.btnEtc.Text = "Прочее";
+            this.btnEtc.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 386);
+            this.Controls.Add(this.btnEtc);
+            this.Controls.Add(this.btnEditObject);
+            this.Controls.Add(this.btnCreateObject);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "New GRK_Admin";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateObject;
+        private System.Windows.Forms.Button btnEditObject;
+        private System.Windows.Forms.Button btnEtc;
     }
 }
 
---
+++ NewGRK_Admin/NewGRK_Admin/grkObjectForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace NewGRK_Admin
+{
+    partial class grkObjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // grkObjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "grkObjectForm";
+            this.Text = "GRK Объект";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

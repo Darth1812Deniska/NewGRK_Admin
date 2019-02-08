573bb9722d9377ce540c7b62058ba42343e1f337
deniska1812 <den93mos@yandex.ru>
2019-02-08 10:39:29 +0300

    Добавление интерфейса для работы  БД

---
+++ NewGRK_Admin/Classes/IGRK_BD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGRK_Admin
+{
+    class IGRK_DB
+    {
+        string ConnectionString { get; set; }
+        string ServerName { get; set; }
+        string DataBaseName { get; set; }
+        string Login { get; set; }
+        string Password { get; set; }
+
+        bool TestConnection();
+        bool TestConnection(string conString);
+        bool TestConnection(string serverName, string dbName, string login, string password);
+    }
+}
